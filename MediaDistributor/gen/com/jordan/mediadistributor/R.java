99fdfc3cca5198bf51b8e6b6f66f83fe071dfd61
Jordan <jrhochstetler@gmail.com>
2014-03-13 18:38:10 -0500

    Added old connections to the server

--- MediaDistributor/gen/com/jordan/mediadistributor/R.java
+++ MediaDistributor/gen/com/jordan/mediadistributor/R.java
@@ -14,9 +14,27 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int adminoption_detail=0x7f060002;
-        public static final int adminoption_detail_container=0x7f060000;
-        public static final int adminoption_list=0x7f060001;
+        public static final int action_settings=0x7f070014;
+        public static final int add_user=0x7f07000c;
+        public static final int adminoption_detail=0x7f070002;
+        public static final int adminoption_detail_container=0x7f070000;
+        public static final int adminoption_list=0x7f070001;
+        public static final int authButton=0x7f070013;
+        public static final int header=0x7f07000d;
+        public static final int horizontalView=0x7f070004;
+        public static final int layout=0x7f070003;
+        public static final int login=0x7f070011;
+        public static final int password=0x7f070010;
+        public static final int register=0x7f070012;
+        public static final int row_title_Email=0x7f070009;
+        public static final int row_title_Facebook_ID=0x7f07000b;
+        public static final int row_title_Password=0x7f070008;
+        public static final int row_title_User_ID=0x7f070006;
+        public static final int row_title_User_Type=0x7f07000a;
+        public static final int row_title_Username=0x7f070007;
+        public static final int table_layout_user=0x7f070005;
+        public static final int user_input_layout=0x7f07000e;
+        public static final int username=0x7f07000f;
     }
     public static final class layout {
         public static final int activity_adminoption_detail=0x7f030000;
@@ -38,10 +56,18 @@ public final class R {
         public static final int activity_adminoption_list=0x7f030001;
         public static final int activity_adminoption_twopane=0x7f030002;
         public static final int fragment_adminoption_detail=0x7f030003;
+        public static final int main_admin=0x7f030004;
+        public static final int main_login=0x7f030005;
+        public static final int main_member=0x7f030006;
+    }
+    public static final class menu {
+        public static final int main=0x7f060000;
     }
     public static final class string {
+        public static final int action_settings=0x7f040001;
         public static final int app_name=0x7f040000;
-        public static final int title_adminoption_detail=0x7f040001;
+        public static final int hello_world=0x7f040002;
+        public static final int title_adminoption_detail=0x7f040003;
     }
     public static final class style {
         /** 
